2a0f9cf6abbf4dc5a7d1fd70beceb93bfc55412e
Pedro Gallego <pedrogallego@correo.ugr.es>
2020-10-23 11:01:15 +0200

    He copiado y pegado la clase de maps y localizacion

---
+++ app/src/main/java/com/example/sites/Localizacion.kt
@@ -0,0 +1,58 @@
+package com.example.sites
+
+import android.location.Location
+import android.location.LocationProvider
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import android.location.LocationListener
+
+class Localizacion: LocationListener {
+
+    var mainActivity = MainActivity()
+    var tvMensaje: TextView? =null
+
+    override fun onLocationChanged(location: Location) {
+        //este método se ejecuta cuando el gps recibe nuevas coordenadas
+        var texto="Mi ubicación es: \nLatitud = ${location.latitude}\nLongitud = ${location.longitude}"
+        tvMensaje?.text = texto
+
+        mapa(location.latitude, location.longitude)
+    }
+
+    fun mapa(lat: Double, lon: Double){
+        //Fragment del mapa
+        var fragment = FragmentMaps()
+
+        var bundle= Bundle()
+        bundle.putDouble("lat",lat)
+        bundle.putDouble("lon",lon)
+        fragment.arguments = bundle
+
+        var fragmentManager: FragmentManager =this.mainActivity.supportFragmentManager
+        var fragmentTransaction: FragmentTransaction=fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment,fragment, null)
+        fragmentTransaction.commit()
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?){
+        if(status== LocationProvider.AVAILABLE){
+            Log.d("debug","LocationProvider.AVAILABLE")
+        }else if(status== LocationProvider.OUT_OF_SERVICE){
+            Log.d("debug","LocationProvider.OUT_OF_SERVICE")
+        }else if(status== LocationProvider.TEMPORARILY_UNAVAILABLE){
+            Log.d("debug","LocationProvider.TEMPORARILY_UNAVAILABLE")
+        }
+    }
+
+    override fun onProviderEnabled(provider: String) {
+        tvMensaje?.text="GPS Activado"
+    }
+
+    override fun onProviderDisabled(provider: String) {
+        tvMensaje?.text="GPS Desactivado"
+    }
+
+}
--- app/src/main/java/com/example/sites/MapsActivity.kt
+++ app/src/main/java/com/example/sites/MapsActivity.kt
@@ -1,43 +1,47 @@
 package com.example.sites
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+class FragmentMaps: SupportMapFragment(), OnMapReadyCallback{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+    var lat: Double=0.0
+    var lon: Double=0.0
+
+
+    override fun onCreateView(p0: LayoutInflater, p1: ViewGroup?, p2: Bundle?): View? {
+        var rootView=super.onCreateView(p0, p1, p2)
+
+        if(getArguments()!=null){
+            this.lat= arguments!!.getDouble("lat")
+            this.lon=arguments!!.getDouble("lon")
+        }
+
+        getMapAsync(this)
+
+        return rootView
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    override fun onMapReady(googleMap: GoogleMap?) {
+        var latLng = LatLng(lat,lon)
+        var zoom: Float = 17F
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (googleMap != null) {
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom))
+
+            googleMap.uiSettings.isZoomControlsEnabled=true
+
+            googleMap.addMarker(MarkerOptions().position(latLng))
+
+            var settings: UiSettings=googleMap.uiSettings
+
+            settings.isZoomControlsEnabled=true
+        }
     }
 }
