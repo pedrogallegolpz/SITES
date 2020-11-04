fc65ddb749a88a71e054d3b6d48d8cfaa4a04f8a
Victor Manuel Arroyo Martin <victorarroyo@correo.ugr.es>
2020-11-04 19:49:13 +0100

    Añadido código inicial de mirar a dos lugares y cambiar el textView

--- app/src/main/java/com/example/sites/MainActivity.kt
+++ app/src/main/java/com/example/sites/MainActivity.kt
@@ -15,6 +15,10 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.provider.Settings
 import android.util.Log
 import android.view.TextureView
 import android.view.View
@@ -22,6 +26,9 @@ import android.view.animation.Animation
 import android.view.animation.RotateAnimation
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import java.lang.Math.*
 
 class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedListener,
     SensorEventListener {
@@ -40,6 +47,19 @@ class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedL
     // image compass
     private var imview: ImageView?=null
 
+
+    var tvMensaje: TextView?=null
+    val MIN_TIME: Long=1000000
+    var local:Localizacion?=null
+    var locationManager:LocationManager?=null
+    var longit:Double=0.0
+    var latit:Double= 0.0
+
+    var posPrueba1lat:Double=37.159181
+    var posPrueba1lon:Double=-3.608803
+    var posPrueba2lat:Double=37.159698
+    var posPrueba2lon:Double=-3.6065912
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,6 +69,15 @@ class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedL
         initData()
 
         gestureSetup()
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1000)
+        }else{
+            iniciarLocalizacion()
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        }
+
     }
 
     private fun gestureSetup(){
@@ -87,6 +116,12 @@ class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedL
                 finish()
             }
         }
+        if(requestCode==1000){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                iniciarLocalizacion()
+                return
+            }
+        }
     }
 
     override fun onResume() {
@@ -120,6 +155,7 @@ class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedL
 
         imview?.startAnimation(rotateAnimation)
         currentDegree= (-degree).toFloat()
+        mirandoHacia(currentDegree)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -130,4 +166,62 @@ class MainActivity : AppCompatActivity(), GestureOverlayView.OnGesturePerformedL
         mSensorManager=getSystemService(Context.SENSOR_SERVICE) as SensorManager?
         imview=findViewById(R.id.imgCompass)
     }
+
+
+    fun iniciarLocalizacion(){
+        locationManager= getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        local = Localizacion()
+
+        local?.tvMensaje = tvMensaje
+
+        val gpsEnabled: Boolean= locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if(!gpsEnabled){
+            var intent: Intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1000)
+
+            return;
+        }
+        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, 0F, local as LocationListener)
+        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, 0F, local as LocationListener)
+
+        ubicaciones?.setText("Localización Agregada")
+
+    }
+
+    //define the listener
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            /*val sb = StringBuilder()
+            val ubi = ("Longitud:" + location.longitude + " Latitud: " + location.latitude)
+            sb.append(ubicaciones.text).append(System.lineSeparator()).append(ubi)
+            ubicaciones.text=sb.toString()*/
+            ubicaciones.text=("Longitud:" + location.longitude + " Latitud: " + location.latitude)
+            longit=location.longitude
+            latit=location.latitude
+        }
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    fun mirandoHacia(deg: Float){
+        var angulo:Double=Math.atan2(sin(posPrueba1lon-longit)*cos(posPrueba1lat), cos(latit)*sin(posPrueba1lat)-sin(latit)*cos(posPrueba1lat)*cos(posPrueba1lon-longit))
+        var degg=-deg
+        var angulo2:Double=Math.atan2(sin(posPrueba2lon-longit)*cos(posPrueba2lat), cos(latit)*sin(posPrueba2lat)-sin(latit)*cos(posPrueba2lat)*cos(posPrueba2lon-longit))
+        angulo+=PI
+        angulo2+=PI
+        if(abs(toRadians(degg.toDouble())-angulo)<PI/4){
+            mirandoa.text="Mirando hacia Posicion 1"
+        }else if(abs(toRadians(degg.toDouble())-angulo2)<PI/4){
+            mirandoa.text = "Mirando hacia Posicion 2"
+        }else
+            mirandoa.text = ""
+    }
+
 }
