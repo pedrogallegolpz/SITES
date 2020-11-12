cb92d04ccdec6dddd291dc62bd0376eb6ee7cac3
sergiocgdl <sergiocabezasgdl@gmail.com>
2020-11-12 11:06:15 +0100

    fallo tonto

--- app/src/main/java/com/example/sites/Zona.kt
+++ app/src/main/java/com/example/sites/Zona.kt
@@ -111,8 +111,8 @@ internal object Zona {
     )
     val arrayZonas = arrayOf(centro, plazaToros, chana, cerrillo, vega, zaidin, juventud, carreteraSierra, norte,
     cartuja, albaicin, alhambra, realejo, generalife, sacromonte)
-    val arrayNombres = arrayOf("Zona Centro", "Zona Plaza de Toros", "Zona Chana", "Zona Cerrillo de Maracena", "Zona de la Vega",
-    "Zona Zaidín", "Zona Estadio de la Juventud" "Zona Carretera de la Sierra", "Zona Norte", "Zona Cartuja", "Zona Albaicin", "Zona Alhambra", "Zona Realejo", "Zona Generalife",
+    val arrayNombres = arrayOf("Zona Centro", "Zona Plaza de Toros", "Zona Chana", "Zona Cerrillo de Maracena", "Zona de la vega",
+    "Zona Zaidín", "Zona Estadio de la Juventud", "Zona Carretera de la Sierra", "Zona Norte", "Zona Cartuja", "Zona Albaicin", "Zona Alhambra", "Zona Realejo", "Zona Generalife",
     "Zona Sacromonte")
 
 
