966e9f948019e4064c05067e8a0013dce91ab47d
Pedro Gallego <pedrogallego@correo.ugr.es>
2020-11-12 11:04:48 +0100

    He puesto una coma en lo del estadio juvntud

--- app/src/main/java/com/example/sites/Zona.kt
+++ app/src/main/java/com/example/sites/Zona.kt
@@ -112,7 +112,7 @@ internal object Zona {
     val arrayZonas = arrayOf(centro, plazaToros, chana, cerrillo, vega, zaidin, juventud, carreteraSierra, norte,
     cartuja, albaicin, alhambra, realejo, generalife, sacromonte)
     val arrayNombres = arrayOf("Zona Centro", "Zona Plaza de Toros", "Zona Chana", "Zona Cerrillo de Maracena", "Zona de la Vega",
-    "Zona Zaidín", "Zona Estadio de la Juventud" "Zona Carretera de la Sierra", "Zona Norte", "Zona Cartuja", "Zona Albaicin", "Zona Alhambra", "Zona Realejo", "Zona Generalife",
+    "Zona Zaidín", "Zona Estadio de la Juventud", "Zona Carretera de la Sierra", "Zona Norte", "Zona Cartuja", "Zona Albaicin", "Zona Alhambra", "Zona Realejo", "Zona Generalife",
     "Zona Sacromonte")
 
 
