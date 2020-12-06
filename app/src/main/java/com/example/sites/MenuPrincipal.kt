dcc82fb19830dc1b84a8e6311cec3e562d17a330
Pedro Gallego <pedrogallego@correo.ugr.es>
2020-12-06 21:48:14 +0100

    Ya va el chatbot

--- app/src/main/java/com/example/sites/MenuPrincipal.kt
+++ app/src/main/java/com/example/sites/MenuPrincipal.kt
@@ -3,7 +3,6 @@ package com.example.sites
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.view.View
 import android.widget.ImageButton
 
