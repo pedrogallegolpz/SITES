236917e94bca3d8b2846a79f49ea2317eb174393
Álvaro Beltrán Camacho <alvaro98@correo.ugr.es>
2021-02-07 11:53:34 +0100

    images dest no se usan

--- app/src/main/java/com/example/sites/ListaMiradores.kt
+++ app/src/main/java/com/example/sites/ListaMiradores.kt
@@ -16,8 +16,6 @@ class ListaMiradores : AppCompatActivity() {
     var  miradores:Miradores = Miradores
     private lateinit var listamiradores: ListView
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_miradores)
